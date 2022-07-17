ae5c7c9f467cba759a1403338b985b42c0eaf12e
Allison <67673392+Allybe@users.noreply.github.com>
2022-07-17 01:32:51 -0500

    Ban command

---
+++ commands/ban.ts
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { Permissions } = require('discord.js');
+
+module.exports.help = {
+    name: "ban",
+    cat: "Mod",
+    description: "Bans a mentioned user",
+    aliases: "",
+    data: new SlashCommandBuilder().setName("ban").setDescription("Bans a mentioned user"),
+};
+
+//If interaction command
+module.exports.interaction = async (interaction, client) => {
+    //Not doing this - Ally
+};
+
+//If normal command
+module.exports.run = async (client, message, args) => {
+    if (message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
+        var userID;
+
+        if (/<@(\d+)>/g.test(args[0]) === true) {
+            let t = /<@(\d+)>/g.exec(args[0]);
+
+            userID = t[1]; //This won't be null, trust me - Ally
+        } else {
+            userID = args[0];
+        }
+
+        var i = client.users.cache.find((i1) => i1 == userID);
+
+        if (i === undefined) {
+            //Call for help
+            message.channel.send("The user referenced is invaild");
+            return;
+        }
+
+        message.guild.members.ban(userID)
+            .then(j => {
+                message.channel.send(`:white_check_mark: User was banned from ${message.guild.name}`);
+            });
+    }
+};
