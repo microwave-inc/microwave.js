adcb6e56fa26afd27ed8c2f181b0193e2c8ada7e
galaxine-senpai <insta.is.good1234@gmail.com>
2022-07-16 23:05:39 -0700

    Status :sunglasses:

--- index.ts
+++ index.ts
@@ -38,6 +38,7 @@ fs.readdir("./commands/", async (err, files) => {
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("with code", { type: "PLAYING" }); //I'll make a list for this later -Ayden
 });
 
 //When slash commands are ran
