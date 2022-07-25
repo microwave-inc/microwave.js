bf480ecb227d21ff1342d3c3f77772679f351963
galaxine-senpai <insta.is.good1234@gmail.com>
2022-07-25 16:36:24 -0700

    Help command

---
+++ commands/help.ts
@@ -0,0 +1,87 @@
+const { SlashCommandBuilder } = require("@discordjs/builders"); 
+ const fs = require("fs"); 
+ const { MessageEmbed } = require("discord.js"); 
+  
+ module.exports.help = { 
+   name: "help", 
+   cat: "Info",
+   description: "Help command", 
+   aliases: "", 
+   data: new SlashCommandBuilder() 
+     .setName("help") 
+     .setDescription("Help command"), 
+ }; 
+//thank you ally for the help command base, did a bit of fixing tho -Ayden
+ //If interaction command 
+ module.exports.interaction = async (interaction, client, onlineSelector, idLogger, pollManager) => { 
+    var aEmbed = new MessageEmbed().setTitle("All commands");
+   
+    fs.readdir("commands/", async (err, files) => { 
+      if (err) throw err; 
+   
+      var fileName = files.filter((files) => files.split(".").pop() === "ts"); 
+   
+        //Adding commands to the help command  
+      await fileName.forEach((fileName) => { 
+        let properties = require(`../commands/${fileName}`); 
+   
+        if (properties.help.args) { 
+          let formatedArgs = properties.help.args.split(",").join(" "); 
+          aEmbed.addFields({ 
+            name: `/${properties.help.name} \`${formatedArgs}\``, 
+            value: `${properties.help.description}`, 
+          }); 
+        } else { 
+          let properties = require(`../commands/${fileName}`); 
+   
+          aEmbed.addFields({ 
+            name: `/${properties.help.name}`, 
+            value: `${properties.help.description}`, 
+          }); 
+        } 
+      }); 
+   
+      interaction.reply({ embeds: [aEmbed] }); 
+    }); 
+  
+ };
+//If normal command 
+ module.exports.run = async ( 
+   client, 
+   message, 
+   args, 
+   onlineSelector, 
+   idLogger, 
+   pollManager 
+ ) => { 
+   var aEmbed = new MessageEmbed().setTitle("All commands"); 
+   let config = require("../config.json");
+  
+   fs.readdir("commands/", async (err, files) => { 
+     if (err) throw err; 
+  
+     var fileName = files.filter((files) => files.split(".").pop() === "ts"); 
+  
+       //Adding commands to the help command  
+     await fileName.forEach((fileName) => { 
+       let properties = require(`../commands/${fileName}`); 
+  
+       if (properties.help.args) { 
+         let formatedArgs = properties.help.args.split(",").join(" "); 
+         aEmbed.addFields({ 
+           name: `${config.prefix}${properties.help.name} \`${formatedArgs}\``, 
+           value: `${properties.help.description}`, 
+         }); 
+       } else { 
+         let properties = require(`../commands/${fileName}`); 
+  
+         aEmbed.addFields({ 
+           name: `${config.prefix}${properties.help.name}`, 
+           value: `${properties.help.description}`, 
+         }); 
+       } 
+     }); 
+  
+     message.channel.send({ embeds: [aEmbed] }); 
+   }); 
+ };
