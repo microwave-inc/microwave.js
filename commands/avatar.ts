673107423ad7d6b5759f26962dbfadbdd9e98afc
galaxine-senpai <insta.is.good1234@gmail.com>
2022-07-27 16:17:53 -0700

    Did avatar the lazy way

--- commands/avatar.ts
+++ commands/avatar.ts
@@ -6,13 +6,24 @@ module.exports.help = {
     cat: "Discord",
     description: "Get the avatar of you or someone else",
     aliases: "",
-    data: new SlashCommandBuilder().setName("avatar").setDescription("Get the avatar of you or someone else"),
+    data: new SlashCommandBuilder().setName("avatar").setDescription("Get the avatar of you or someone else").addUserOption(option => {
+        return option
+            .setName("user")
+            .setDescription("The user to get the avatar of")
+            .setRequired(true)
+    })
 };
 
 //If interaction command
 module.exports.interaction = async (interaction, client) => {
     //Ez fix for ya -Ayden
-    return interaction.reply({ content: "Uh oh, this has not been implemented yet.", ephemeral: true });
+    let userID = interaction.options.getUser("user");
+    const embed = new MessageEmbed().setColor("#26a6d9");
+
+    embed
+        .setTitle(userID.username + "'s Avatar")
+        .setImage(userID.displayAvatarURL({ format: "jpg", size: 4096 }));
+    interaction.reply({  embeds: [embed] });
 };
 
 //If normal command
