3517685b3646f48a208b6305f84e688e0ba26447
galaxine-senpai <insta.is.good1234@gmail.com>
2022-07-27 22:14:19 -0700

    update about command

--- commands/about.ts
+++ commands/about.ts
@@ -28,3 +28,24 @@ module.exports.interaction = async (interaction, client) => {
         .setTimestamp()
     await interaction.reply({ embeds: [embed] });
 }
+
+module.exports.run = async (client, message) => {
+    const embed = new MessageEmbed()
+    .setColor("#FFFFFE")
+    .setTitle("About")
+    .setURL("https://microwavebot.tech/discord")
+    .addFields(
+        { name: "Version", value: "1.0.0", inline: true },
+        { name: "Discord Library", value: "Discord.js v13", inline: true },
+        { name: "Node.js", value: "v16.16.0", inline: true },
+        { name: "Dev Team", value: "\u200B" },
+        { name: "Ayden", value: "Founder and Lead bot developer", inline: true },
+        { name: "Fonta22", value: "Assistant lead bot developer", inline: true },
+        { name: "Ally", value: "Bot developer", inline: true },
+        { name: "Sopy", value: "Bot developer", inline: true },
+        { name: "Yapudjus", value: "Bot developer", inline: true },
+    )
+    .setTimestamp()
+
+    message.channel.send({ embeds: [embed] });
+}
