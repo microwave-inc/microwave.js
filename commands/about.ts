ec6d7373e8c1e17c55d61fd1bc0bfee2ed2e1714
galaxine-senpai <insta.is.good1234@gmail.com>
2022-07-27 22:12:45 -0700

    Added /about

---
+++ commands/about.ts
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+
+module.exports.help = {
+    name: "about", 
+    cat: "Info",
+    description: "Get information about the bot",
+    aliases: "",
+    data: new SlashCommandBuilder().setName("about").setDescription("Get information about the bot"),
+}
+
+module.exports.interaction = async (interaction, client) => {
+    const embed = new MessageEmbed()
+        .setColor("#FFFFFE")
+        .setTitle("About")
+        .setURL("https://microwavebot.tech/discord")
+        .addFields(
+            { name: "Version", value: "1.0.0", inline: true },
+            { name: "Discord Library", value: "Discord.js v13", inline: true },
+            { name: "Node.js", value: "v16.16.0", inline: true },
+            { name: "Dev Team", value: "\u200B" },
+            { name: "Ayden", value: "Founder and Lead bot developer", inline: true },
+            { name: "Fonta22", value: "Assistant lead bot developer", inline: true },
+            { name: "Ally", value: "Bot developer", inline: true },
+            { name: "Sopy", value: "Bot developer", inline: true },
+            { name: "Yapudjus", value: "Bot developer", inline: true },
+        )
+        .setTimestamp()
+    await interaction.reply({ embeds: [embed] });
+}
