eda2262dc16b538370c1910b33de3d440a513422
gawaxine-senpai <ayden@microwavebot.tech>
2022-12-28 13:32:26 -0800

    Non descriptive commit

--- deploy.js
+++ deploy.js
@@ -4,11 +4,11 @@ const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.ts'));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	commands.push(command.help.toJSON());
+	commands.push(JSON.stringify(command.help));
 }
 
 const rest = new REST({ version: '10' }).setToken(token);
