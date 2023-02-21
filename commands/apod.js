03738e74ea33156bfb4f909e863774257c949f86
galaxine-senpai <insta.is.good1234@gmail.com>
2023-02-21 09:25:28 -0800

    APOD command (untested)

---
+++ commands/apod.js
@@ -0,0 +1,80 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed, version} = require("discord.js");
+const config = require("../config.json");
+const fetch = require("node-fetch");
+
+module.exports.help = {
+    name: "apod",
+    cat: "Fun",
+    description: "Get the Astronomy Picture of the Day",
+    aliases: "",
+    data: new SlashCommandBuilder().setName("apod").setDescription("Get the Astronomy Picture of the Day").addIntegerOption(option => {
+        return option
+            .setName("Year")
+            .setDescription("The year to get the APOD from")
+        }).addIntegerOption(option => {
+            return option
+                .setName("Month")
+                .setDescription("The month to get the APOD from")
+        }).addIntegerOption(option => {
+            return option
+                .setName("Day")
+                .setDescription("The day to get the APOD from")
+        })
+        };
+
+//If interaction command
+module.exports.interaction = async (interaction, client) => {
+    const apikey = config.nasa; // Not sure if this works.
+    var api
+
+    // Date options
+    let year = interaction.options.getInteger("Year");
+    let month = interaction.options.getInteger("Month");
+    let day = interaction.options.getInteger("Day");
+
+    if (year != null) {
+        if (month != null) {
+            if (day != null) {
+                let api = `https://api.nasa.gov/planetary/apod?api_key=${apikey}&date=${year}-${month}-${day}`;
+
+                if (year < 1995) { // Check if the date set is before June 16th 1995 (The first APOD) // Only check if a year month and day has been set
+                    if (month < 6) {
+                        if (day < 16) {
+                            interaction.reply("Sorry, but the APOD API does not support dates before June 16, 1995.");
+                        }
+                    }
+                }
+    } else {
+        let api = `https://api.nasa.gov/planetary/apod?api_key=${apikey}`
+    }
+} else {   
+    let api = `https://api.nasa.gov/planetary/apod?api_key=${apikey}`;
+}
+    } else {
+        let api = `https://api.nasa.gov/planetary/apod?api_key=${apikey}`;
+    } // This is a mess, but it should work..... I need to K.I.S.S. (Keep It Simple Stupid)
+
+
+
+    fetch(api)
+    .then(response => response.json()) // Get the json
+    .then(data => { // Set all the vars from the JSON file
+        image = data.hdurl;
+        title = data.title;
+        explanation = data.explanation; // This will prob not be used
+        date = data.date;
+
+        const embed = new MessageEmbed()
+        .setTitle(title)
+        .setDescription(`date: ${date}`)
+        .setImage(image)
+
+        interaction.reply({embeds: [embed]}); // Hopefully this sends the embed
+    });
+};
+
+//If normal command
+module.exports.run = async (client, message, args) => {
+    await message.channels.send("Hello! We have moved fully away from prefixed commands, please use the slash commands instead!")
+};
