ae0f995f3d1e279d37212f48316cbe7659992f91
galaxine-senpai <insta.is.good1234@gmail.com>
2023-02-28 21:54:46 -0800

    wow

--- commands/Owner/eval.js
+++ commands/Owner/eval.js
@@ -15,12 +15,26 @@ module.exports.help = {
     })};
 
 module.exports.interaction = async (interaction, client) => {
-if (interaction.user.id !== config.ownerID) return interaction.reply({ content: "You are not a wizard harry!", ephemeral: true }); // Need to get the login info of one of my alts for this
-if (interaction.options.getString("code") === "") return interaction.reply({ content: "You need to provide code to evaluate!", ephemeral: true });
-code = interaction.options.getString("code");
-    start = (Date.now()),
-    evalresponse = eval(code),
-    time = Date.now() - start;
-
-message.channel.send(`**Evaluated:**\n\`\`\`js\n${stringtoeval}\`\`\`**Response:**\n\`\`\`js\n${evalresponse}\`\`\`In ${time}ms.`)
+if (interaction.user.id != config.ownerID) {
+    if (interaction.options.getString("code") === "") 
+    {
+        return interaction.reply({ content: "You need to provide code to evaluate!", ephemeral: true });
+    }
+    else {
+    code = interaction.options.getString("code");
+        if (code == "const config = require(\"../../config.json\");") {
+            return interaction.reply({ content: "You are not a wizard harry!", ephemeral: true });
+        }
+        start = (Date.now()),
+        evalresponse = eval(code),
+        time = Date.now() - start;
+    
+    return interaction.reply(`**Response:**\n\`\`\`js\n${evalresponse}\`\`\`In ${time}ms.`)
+    };
+}
+else {
+    return interaction.reply({ content: "You are not a wizard harry!", ephemeral: true });
+}
 };
+
+// console.log("test")
