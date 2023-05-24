6ea77e3a22ad81d82aedc8bfeb22d5193d0fbe9f
galaxine-senpai <insta.is.good1234@gmail.com>
2023-05-24 12:39:07 -0700

    Suggest command update Suggest command now uses a modal!

--- commands/Utility/suggest.js
+++ commands/Utility/suggest.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
+const { MessageActionRow, Modal, TextInputComponent } = require('discord.js');
 
 module.exports.help = {
     name: "suggest",
@@ -7,33 +8,49 @@ module.exports.help = {
     description: "Suggest something to the devs",
     aliases: "",
     cmdid: "1080020862335455325",
-    data: new SlashCommandBuilder().setName("suggest").setDescription("Suggest something to the devs").addStringOption( option => {
-        return option
-        .setName("suggestion")
-        .setDescription("The suggestion")
-        .setRequired(true);
-    }).addStringOption( option => {
-        return option
-        .setName("type")
-        .setDescription("The type of suggestion")
-        .setRequired(true)
-        .addChoices({name: "Bot", value: "Bot"}, {name: "Website", value: "Website"}, {name: "Other", value: "Other"})
-    })
+    data: new SlashCommandBuilder().setName("suggest").setDescription("Suggest something to the devs")
 };
 
 module.exports.interaction = async (interaction, client) => {
-    const suggestion = interaction.options.getString("suggestion");
-    const type = interaction.options.getString("type");
-    const embed = new MessageEmbed()
-    .setTitle("Suggestion")
-    .setAuthor({ name: `${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL({ format: "jpg"})})
-    //.setThumbnail(interaction.user.displayAvatarURL({ format: "jpg"})) // Was changed out for .setAuthor
-    .setDescription(suggestion + "\nFor: " + type +"\n ID (User): " + interaction.user.id)
-    .setColor("#0099ff")
-    .setTimestamp()
-    //.setfooter({ text: `Suggested by ${interaction.user.username}` });
-    client.channels.cache.get("1002019218407051325").send({ embeds: [embed] });
-    interaction.reply({ content: "Your suggestion has been sent to the devs", ephemeral: true });
+    const suggestionschannel = client.channels.cache.get("1002019218407051325")
+
+    const modal = new Modal()
+        .setCustomId('suggestionmodal')
+        .setTitle('Suggestion')
+    const suggestion = new TextInputComponent()
+        .setCustomId('suggestion')
+        .setLabel('Suggestion')
+        .setPlaceholder('Suggestion (DO NOT INCLUDE MORE THAN ONE SUGGESTION)')
+        .setMinLength(10)
+        .setStyle('PARAGRAPH')
+        .setRequired(true)
+
+    const firstactionrow = new MessageActionRow().addComponents(suggestion)
+    modal.addComponents(firstactionrow)
+    await interaction.showModal(modal);
+    const submitted = await interaction.awaitModalSubmit({
+        time: 60000,
+        filter: i => i.user.id === interaction.user.id,
+    }).catch(error => {
+        // Catch any Errors that are thrown (e.g. if the awaitModalSubmit times out after 60000 ms)
+        console.error(error)
+        return null
+    });
+
+    if (submitted) {
+        // interaction.deferReply({ ephemeral: true }); // Turns out you can't defer a modal reply
+        const suggestiontext = submitted.fields.getTextInputValue('suggestion');
+        const suggestionembed = new MessageEmbed()
+        .setTitle("Suggestion")
+        .setAuthor({ name: `${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL({ format: "jpg" }) })
+        .setDescription(suggestiontext + "\n ID (User): " + interaction.user.id)
+        .setColor("#0099ff")
+        .setTimestamp()
+        await suggestionschannel.send({ embeds: [suggestionembed] });
+        await submitted.reply({ content: "Your suggestion has been sent to the devs", ephemeral: true });
+    } else {
+        await interaction.followUp({ content: "You didn't submit a suggestion in time.", ephemeral: true });
+    }
 }
 
 module.exports.run = async (message, client) => {
