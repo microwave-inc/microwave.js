a6f6edbdc6f335882740ac471b4240093225518d
galaxine-senpai <insta.is.good1234@gmail.com>
2023-07-23 00:40:47 -0700

    Steam command update Added the ablility to search for a user via their steam url

--- commands/Info/steam.js
+++ commands/Info/steam.js
@@ -10,13 +10,35 @@ module.exports.help = {
     data: new SlashCommandBuilder()
         .setName('steam')
         .setDescription('Get a steam profile')
-        .addStringOption(option => option.setName('steamid').setDescription('Steam ID').setRequired(true))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('steamid')
+                .setDescription('Get a steam profile via the steam ID')
+                .addStringOption(option =>
+                    option
+                        .setName('steamid')
+                        .setDescription('The steam ID of the user')
+                        .setRequired(true)
+                ))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('steamlink')
+                .setDescription('Get a steam profile via the custom steam link')
+                .addStringOption(option =>
+                    option
+                        .setName('steamlink')
+                        .setDescription('The steam link of the user')
+                        .setRequired(true)
+                ))
 }
 
 module.exports.interaction = async (interaction, client) => {
     const steamid = interaction.options.getString("steamid"); // Get the steam ID.
     const api = await fetch("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + steamapikey + '&steamids=' + steamid) // Get the steam profile.
     const data = await api.json(); // Get the data.
+    const subcommand = interaction.options.getSubcommand(); // Get the subcommand.
+
+    if (subcommand == 'steamid') {
 
     /**
      * Example output:
@@ -77,5 +99,53 @@ module.exports.interaction = async (interaction, client) => {
             { name: 'Last Logoff', value: '<t:'+data.response.players[0].lastlogoff+':R>', inline: true },
         )
     return interaction.reply({ embeds: [embed] })
+    } else if (subcommand == 'steamlink') {
+        const steamlink = interaction.options.getString("steamlink"); // Get the steam link.
+        const steamid = await fetch("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=" + steamapikey + '&vanityurl=' + steamlink) // Get the steam ID.")
+        const steamidjson = await steamid.json(); // Get the steam ID data.
+        
+        
 
+        if (steamidjson.response.success === 42) { // Check if the steam link is invalid.
+            const embed = new MessageEmbed()
+                .setTitle('Steam Profile')
+                .setDescription('Steam profile not found.')
+                .setColor('RED')
+            return interaction.reply({ embeds: [embed] })
+        };
+        // Wait till we can verify that the steam link is valid.
+        const api = await fetch("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + steamapikey + '&steamids=' + steamidjson.response.steamid) // Get the steam profile.
+        const data = await api.json(); // Get the data.
+
+        if (!api.ok) { return interaction.reply({ content: 'The API did not respond correctly, please try again later', ephemeral: true }); }; // Check if the API responds with anything other than 200 (OK)
+        if (!data.response.players[0].steamid) { // Check if ID is invalid
+            const embed = new MessageEmbed()
+                .setTitle('Steam Profile')
+                .setDescription('Steam profile not found.')
+                .setColor('RED')
+            return interaction.reply({ embeds: [embed] })
+        };
+        if (!data.response.communityvisibilitystate == 3 ) {// Check if the profile is private.
+            const embed = new MessageEmbed()
+                .setTitle('Steam Profile')
+                .setDescription('Steam profile is private.')
+                .setColor('RED')
+            return interaction.reply({ embeds: [embed] })
+        };
+        const embed = new MessageEmbed()
+            .setTitle('Steam Profile')
+            .setDescription('Steam profile found.')
+            .setColor('GREEN')
+            .setThumbnail(data.response.players[0].avatarfull)
+            .addFields(
+                { name: 'Username', value: data.response.players[0].personaname, inline: true },
+                { name: 'Real Name', value: data.response.players[0].realname, inline: true },
+                { name: 'Country', value: data.response.players[0].loccountrycode, inline: true },
+                { name: 'Profile URL', value: data.response.players[0].profileurl, inline: true },
+                { name: 'Time Created', value: '<t:'+data.response.players[0].timecreated+':R>', inline: true },
+                { name: 'Last Logoff', value: '<t:'+data.response.players[0].lastlogoff+':R>', inline: true },
+                )
+            return interaction.reply({ embeds: [embed] })
+
+    }
 };
