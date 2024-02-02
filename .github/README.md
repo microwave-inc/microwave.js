db09d3e71f176f6c90ae6f86a9c1adb770b40344
galaxine-senpai <insta.is.good1234@gmail.com>
2024-02-02 12:54:21 -0800

    Small update Changed stuff regarding better uptime heartbeat Updated config

--- .github/README.md
+++ .github/README.md
@@ -1,4 +1,6 @@
-Config.json layout:
+# README
+
+### Config.json layout
 
 ```json
 {
@@ -12,13 +14,15 @@ Config.json layout:
     "ownerID" : [
     "ID HERE"
     ],
-    "nasa": ""
+    "nasa": "",
+    "heartbeatlink": ""
 }
 ```
 
-**Note:** To obtain a nasa api token go to the [NASA API](https://api.nasa.gov/) website
+> [!NOTE] \
+> To obtain the nasa api key go to https://api.nasa.gov/ and sign up for an api key. \
+> To obtain the heartbeat link go to your https://betteruptime.com/ account and get your heartbeat link.
 
 Make sure to rename `example.config.json` to `config.json` and fill everything in... ClientID and GuildID are for deploying commands
 
 If you need additional help don't be afraid to join the discord!
-
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client, Intents, Collection } = require("discord.js");
 const fs = require("fs");
 
-const config = require("./config.json");
+const { token, heartbeatlink, activity } = require("./config.json");
 
 const client = new Client({
   intents: [
@@ -16,11 +16,15 @@ const client = new Client({
 
 client.commands = new Collection();
 
-const commandFolders = fs.readdirSync('./commands');
-const handlerFiles = fs.readdirSync('./handlers').filter(file => file.endsWith('.js'));
+const commandFolders = fs.readdirSync("./commands");
+const handlerFiles = fs
+  .readdirSync("./handlers")
+  .filter((file) => file.endsWith(".js"));
 
 for (const folder of commandFolders) {
-  const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+  const commandFiles = fs
+    .readdirSync(`./commands/${folder}`)
+    .filter((file) => file.endsWith(".js"));
   for (const file of commandFiles) {
     const command = require(`./commands/${folder}/${file}`);
     const commandName = command.help.name.toLowerCase();
@@ -29,21 +33,29 @@ for (const folder of commandFolders) {
     console.log(`| Command category: ${command.help.cat} |`);
 
     client.commands.set(commandName, command);
-    };
-  };
-console.log(`----------------------------`)
+  }
+}
+console.log(`----------------------------`);
 console.log("Successfully loaded commands to memory");
-
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  if (heartbeatlink === "") {
+    console.log("No heartbeat link provided, bot will not send heartbeats");
+  }
   client.user.setActivity(
-    `${config.activity[Math.round(Math.random() * (config.activity.length - 1))]} | m!help`
+    `${
+      activity[Math.round(Math.random() * (activity.length - 1))]
+    } | m!help`
   );
 
   setInterval(function () {
     client.user.setActivity(
-      `${config.activity[Math.floor(Math.random() * (config.activity.length - 1))]} | m!help`
+      `${
+        activity[
+          Math.floor(Math.random() * (activity.length - 1))
+        ]
+      } | m!help`
     );
   }, 5000);
 });
@@ -58,10 +70,18 @@ client.on("interactionCreate", async (interaction) => {
 
   try {
     await command.interaction(interaction, client);
-    console.log(`Command ${interaction.commandName} was ran by ${interaction.user.tag} (${interaction.user.id})`); // Logs the command
+    console.log(
+      `Command ${interaction.commandName} was ran by ${interaction.user.tag} (${interaction.user.id})`
+    ); // Logs the command
   } catch (error) {
     console.error(error);
-    if (interaction.deferred) {interaction.editReply({ content: "There was an error while executing this command!", ephemeral: true }); return};
+    if (interaction.deferred) {
+      interaction.editReply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+      return;
+    }
     await interaction.reply({
       content: "There was an error while executing this command!",
       ephemeral: true,
@@ -79,4 +99,25 @@ client.on("warn", (info) => {
   console.log(`[INFO] ` + info);
 });
 
-client.login(config.token);
+// A task that runs every 5 minutes to let better uptime know bot is online via heartbeat
+setInterval(async function () {
+  if (heartbeatlink === "none") {
+  }
+
+  await fetch(heartbeatlink, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      bot: client.user.id,
+      status: "online",
+    }),
+  });
+
+  
+
+  console.log("Sent heartbeat to better uptime")
+}, 300000);
+
+client.login(token);
